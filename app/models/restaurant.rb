267c28d33d5de17c0e8920397535e0b0ee702a7b
souraya cassam chenai <sourayacc@gmail.com>
2022-03-02 23:00:54 +0400

    yelp amelioré

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,9 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  CAT = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+
 
   validates :address, presence: true
   validates :name, presence: true
-  validates :category, presence: true, inclusion: {in: ["chinese", "italian", "japanese", "french", "belgian"]}
+  validates :category, presence: true, inclusion: { in: CAT }
 end
